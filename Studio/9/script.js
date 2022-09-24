12ebc5cf59b1b798cf47ee54abcb55f9e8da2b1a
vincent-wijaya <vwij0004@student.monash.edu>
2022-09-25 01:12:08 +1000

    Added studio files and week 9 homework files

---
+++ Studio/9/script.js
@@ -0,0 +1,4 @@
+var map1 = "bubblePlot.vg.json";
+vegaEmbed("#map1", map1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
