07e762330e9aeaead43c303d77a63ac32c3d031e
vincent-wijaya <vwij0004@student.monash.edu>
2022-09-25 01:50:24 +1000

    Minor changes

--- Week 9 Homework/script.js
+++ Week 9 Homework/script.js
@@ -1,9 +1,9 @@
-var circuitsMap = "circuitsMap.vg.json";
+var circuitsMap = "./circuitsMap.vg.json";
 vegaEmbed("#circuitsMap", circuitsMap).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var circuitsEuropeMap = "circuitsEuropeMap.vg.json";
+var circuitsEuropeMap = "./circuitsEuropeMap.vg.json";
 vegaEmbed("#circuitsEuropeMap", circuitsEuropeMap).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
