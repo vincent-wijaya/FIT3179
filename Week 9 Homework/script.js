2889c55f0923a2aaaa487e9765da857568fcc028
vincent-wijaya <vwij0004@student.monash.edu>
2022-10-03 12:46:07 +1100

    Pushed assignment files

--- Week 9 Homework/script.js
+++ Week 9 Homework/script.js
@@ -1,9 +1,9 @@
-var circuitsMap = "https://raw.githubusercontent.com/vincent-wijaya/FIT3179/main/Week%209%20Homework/circuitsMap.vg.json";
+var circuitsMap = "circuitsMap.vg.json";
 vegaEmbed("#circuitsMap", circuitsMap).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var circuitsEuropeMap = "https://raw.githubusercontent.com/vincent-wijaya/FIT3179/main/Week%209%20Homework/circuitsEuropeMap.vg.json";
+var circuitsEuropeMap = "circuitsEuropeMap.vg.json";
 vegaEmbed("#circuitsEuropeMap", circuitsEuropeMap).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
