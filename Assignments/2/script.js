cf15e3383eac212ee5e40b949333cf4c91abefb0
vincent-wijaya <vwij0004@student.monash.edu>
2022-10-06 00:54:19 +1100

    Added new files to assignment 2

--- Assignments/2/script.js
+++ Assignments/2/script.js
@@ -7,3 +7,13 @@ var circuitsEuropeMap = "circuitsEuropeMap.vg.json";
 vegaEmbed("#circuitsEuropeMap", circuitsEuropeMap).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var standings = "standings.vg.json";
+vegaEmbed("#standings", standings).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var standings2022 = "standings2022.vg.json";
+vegaEmbed("#standings2022", standings2022).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
