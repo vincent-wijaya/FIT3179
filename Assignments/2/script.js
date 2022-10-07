4d3bbf418fed2870421a9002e13b34d6c740e06c
Vincent Wijaya <vwij0004@student.monash.edu>
2022-10-07 13:41:55 +1100

    Tried adding zoom by scrolling

--- Assignments/2/script.js
+++ Assignments/2/script.js
@@ -3,10 +3,10 @@ vegaEmbed("#circuitsMap", circuitsMap).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var circuitsEuropeMap = "circuitsEuropeMap.vg.json";
-vegaEmbed("#circuitsEuropeMap", circuitsEuropeMap).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+// var circuitsEuropeMap = "circuitsEuropeMap.vg.json";
+// vegaEmbed("#circuitsEuropeMap", circuitsEuropeMap).then(function(result) {
+// // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
 
 var standings = "standings.vg.json";
 vegaEmbed("#standings", standings).then(function(result) {
