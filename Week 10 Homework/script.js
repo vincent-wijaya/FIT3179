71b292dbd3e1ff4aefcf6366dc12595626972dfe
vincent-wijaya <vwij0004@student.monash.edu>
2022-10-09 23:55:13 +1100

    Added week 10 files

---
+++ Week 10 Homework/script.js
@@ -0,0 +1,24 @@
+var circuitsMap = "circuitsMap.vg.json";
+vegaEmbed("#circuitsMap", circuitsMap).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// var circuitsEuropeMap = "circuitsEuropeMap.vg.json";
+// vegaEmbed("#circuitsEuropeMap", circuitsEuropeMap).then(function(result) {
+// // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+// }).catch(console.error);
+
+var standings = "standings.vg.json";
+vegaEmbed("#standings", standings).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var standings2022 = "standings2022.vg.json";
+vegaEmbed("#standings2022", standings2022).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var driverChampions = "driverChampions.vg.json";
+vegaEmbed("#driverChampions", driverChampions).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
